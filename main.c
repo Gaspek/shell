c02b76d52fa5d44c0dea66f06bac1f727dbc9fe3
TamaTamaTamaTamaTama <133633707+TamaTamaTamaTamaTama@users.noreply.github.com>
2024-05-11 22:15:00 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -8,13 +8,19 @@
 #include "history.h"
 #include "change.h"
 #include "execute.h"
-
 #define MAX_ARGS 100
 
-int main(int argc, char *argv[]) {    
+// Function prototype for signal handler
+void sigquit_handler(int signum);
+
+int main(int argc, char *argv[]) {
+    // Set up signal handler for SIGQUIT
+    signal(SIGQUIT, sigquit_handler);
+    
     char* history_path = get_history_file_path();
     char *args[MAX_ARGS+1];
     char input[BUFSIZ];
+    
     while(printf("%s> ", print_prompt()) && fgets(input, BUFSIZ, stdin) != NULL) {
         if(input[0] == 'h') {
             print_history(history_path);
@@ -40,10 +46,21 @@ int main(int argc, char *argv[]) {
         // Check if the command is "cd"
         if (strcmp(args[0], "cd") == 0) {
             change_dir(args);
-            continue;
+            
         }
         
         // Add code to handle other commands
     }
     return EXIT_SUCCESS;
+}
+
+// Signal handler function for SIGQUIT
+void sigquit_handler(int signum) {
+    if (signum == SIGQUIT) {
+        printf("Received SIGQUIT signal. Printing entire history:\n");
+        char* history_path = get_history_file_path();
+        print_history(history_path);
+        free(history_path);
+        
+    }
 }
