afdfc9a5ed29113e51b71e6c8fa7b621bbd8eeb4
TamaTamaTamaTamaTama <133633707+TamaTamaTamaTamaTama@users.noreply.github.com>
2024-05-11 15:20:41 +0200

    Add files via upload
    Typ w+ zmieniony na a i nie wiem poprawilem te zeby zapisywlao lepiej

--- history.c
+++ history.c
@@ -12,19 +12,19 @@ char* get_history_file_path() {
         fprintf(stderr, "Error: HOME environment variable not set.\n");
         exit(EXIT_FAILURE);
     }
-    char* history_path = malloc(strlen(home_dir) + strlen("/SO/projekt/history_log.txt") + 1);
+    char* history_path = malloc(strlen(home_dir) + strlen("/history_log.txt") + 1);
     if (history_path == NULL) {
         fprintf(stderr, "Error: Memory allocation failed.\n");
         exit(EXIT_FAILURE);
     }
-    sprintf(history_path, "%s/SO/projekt/history_log.txt", home_dir);
+    sprintf(history_path, "%s/history_log.txt\n", home_dir);
     printf("%s", history_path);
     return history_path;
 }
 
 void com_history(char *command) {
     char* history_path = get_history_file_path();
-    FILE *file = fopen(history_path, "w+");
+    FILE *file = fopen(history_path, "a");
     if(file != NULL) 
     {
         fprintf(file, "%s\n", command);
--- main.c
+++ main.c
@@ -4,8 +4,12 @@
 #include <string.h>
 #include "read.h"
 #include "history.h"
+#include "change.h"
 
-int main(int argc, char *argv[]){	
+#define MAX_ARGS 10
+
+int main(int argc, char *argv[]){    
+    char *args[MAX_ARGS +1];
     char command[100];
     char* history_path = get_history_file_path();
     FILE *history_file = fopen(history_path, "r");
@@ -25,12 +29,13 @@ int main(int argc, char *argv[]){
     }
     while(1) 
     {
-        printf("$ ");
+        printf(":) ");
         if(fgets(command, sizeof(command), stdin) == NULL )
         {
             printf("fgets returned NULL\n"); // Add a print statement to check if fgets returns NULL
             break;
         }
+        command[strcspn(command, "\n")] = '\0';
         printf("Command entered: %s\n", command);
         com_history(command);
     }
@@ -45,6 +50,19 @@ int main(int argc, char *argv[]){
         printf("\n");
     }
 
+    char *token = strtok(command, " ");
+    int i = 0;
+    while (token != NULL) {
+        args[i] = token;
+        token = strtok(NULL, " ");
+        i++;
+        if(i >= MAX_ARGS) 
+        {
+            break;
+        }
+    }
+    args[i] = NULL;
+
+
     return EXIT_SUCCESS;
 }
-
